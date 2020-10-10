e472ac76c1a2fb71ceeee29365db632c426aeb12
Danium <Kividanya964@gmail.com>
2020-10-10 19:18:20 +0300

    app is ready

--- TestApi/TestApi/Program.cs
+++ TestApi/TestApi/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TestApi.Models;
+using TestAPI;
 
 namespace TestApi
 {
@@ -6,7 +8,45 @@ namespace TestApi
     {
         static void Main(string[] args)
         {
-            
+            string requestString = "http://tester.consimple.pro";
+            APIManager manager = new APIManager();
+
+            bool isOpen = true;
+            while (isOpen)
+            {
+                string getData = Informer.IsGetData();
+
+                if(getData == "y")
+                {
+                    try
+                    {
+                        var request = manager.GetRequest(requestString);
+                        var response = manager.GetResponse(request);
+                        ProductModel data = manager.GetData<ProductModel>(response);
+                        Informer.ShowProductInfo(data);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Данные введены неверно");
+                }
+
+                string isClose = Informer.IsCloseApp();
+
+                if(isClose == "y")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Данные введены неверно");
+                }
+            }
         }
     }
 }
