014f715792f4633603801eceda5306f3ccc4db6e
Danium <Kividanya964@gmail.com>
2020-10-10 19:14:41 +0300

    response models is ready

---
+++ TestApi/TestApi/Models/IProductModels.cs
@@ -0,0 +1,11 @@
+﻿using Data;
+using System.Collections.Generic;
+
+namespace TestAPI.Models
+{
+    public interface IProductModel 
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
---
+++ TestApi/TestApi/Models/ResponseModels.cs
@@ -0,0 +1,12 @@
+﻿using Data;
+using System.Collections.Generic;
+using TestAPI.Models;
+
+namespace TestApi.Models
+{
+    public class ProductModel : IProductModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
