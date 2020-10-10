fe08a3f95a10727a9124baf42736823a6be11bdc
Danium <Kividanya964@gmail.com>
2020-10-10 19:18:06 +0300

    API manager is ready

---
+++ TestApi/TestApi/APIManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using TestAPI;
+
+namespace TestApi
+{
+    public class APIManager
+    {
+        public WebRequest GetRequest(string address)
+        {
+            var request = WebRequest.Create(address);
+
+            return request;
+        }
+
+        public WebResponse GetResponse(WebRequest request)
+        {
+            var response = request.GetResponse();
+
+            return response;
+        }
+
+        public TModel GetData<TModel>(WebResponse response) where TModel : class
+        {
+            TModel data;
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string json = JsonManager.GetJsonString(reader);
+                    data = JsonManager.ConvertToModel<TModel>(json);
+                }
+            }
+            response.Close();
+
+            if (data == null)
+                throw new Exception("Failed to get data");
+
+            return data;
+        }
+
+    }
+}
