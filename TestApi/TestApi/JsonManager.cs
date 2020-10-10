b20eab916a35f0ed5c6b7a7721fe3bf1440bda6d
Danium <Kividanya964@gmail.com>
2020-10-10 19:16:20 +0300

    json manager is ready

---
+++ TestApi/TestApi/JsonManager.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace TestAPI
+{
+    public static class JsonManager
+    {
+        public static string GetJsonString(StreamReader reader)
+        {
+            string json = reader.ReadToEnd();
+            if (json.Length == 0)
+                throw new Exception("Failed to get JSON data");
+
+            return json;
+        }
+
+        public static TModel ConvertToModel<TModel>(string json) where TModel : class
+        {
+            TModel result = JsonConvert.DeserializeObject<TModel>(json);
+
+            if (result == null)
+                throw new Exception("Failed to convert from JSON data");
+
+            return result;
+        }
+    }
+}
