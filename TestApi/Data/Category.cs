c920415512097c4ffbd700fed3008de532f9900a
Danium <Kividanya964@gmail.com>
2020-10-10 19:15:06 +0300

    models is ready

--- TestApi/Data/Category.cs
+++ TestApi/Data/Category.cs
@@ -1,6 +1,6 @@
-﻿namespace RequestData
+﻿namespace Data
 {
-    public class Category
+    public class Category : IModelBase
     {
         public int Id { get; set; }
         public string Name { get; set; }
---
+++ TestApi/Data/IModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public interface IModelBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
