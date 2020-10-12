dc6b981a92b488bf66a9d55810a5e876aaac5be7
Danium <Kividanya964@gmail.com>
2020-10-12 12:16:22 +0300

    Constants class is added

---
+++ TestApi/Data/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public static class Constants
+    {
+        public static class DisplayManager
+        {
+            public static readonly string GetDataQuestion = "Получить данные с сервера?";
+            public static readonly string CloseAppQuestion = "Закрыть приложение?";
+            public static readonly string ConfirmInput = "Для подтверждения нажмите \"Y\" и нажмите ENTER?";
+        }
+        public static class ErrorMessages
+        {
+            public static readonly string IncorrectInputData = "Данные введены неверно";
+        }
+    }
+}
